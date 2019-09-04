17e4f962c5213804632012295bfdd5f4d1d9abff
Kaja <catcalledkaja@gmail.com>
2019-09-04 16:50:03 +0100

    getting right amount of days still dealing with time

--- date.py
+++ date.py
@@ -10,19 +10,17 @@ date_time_str = "Mon Nov 29 21:18:15 +0000 2010"
 # date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')  
 
 array_date= date_time_str.split(" ")
+del array_date[0]
+del array_date[3]
+string=' '
+list_date = string.join(array_date)
 
-# .pop(4).pop(0)
-print(array_date)
+date_object = datetime.datetime.strptime(list_date, '%b %d %H:%M:%S %Y')
 
-
-# print('Date:', date_time_obj.date())
-# print('Time:', date_time_obj.time())
-# print('Date-time:', date_time_obj)
-# print(date_time_obj)
-
+print(date_object)
 
 date = datetime.datetime.now()
-date2 = date + datetime.timedelta(days=0, seconds=0, microseconds=0,
-                                  milliseconds=0, minutes=+5, hours=0, weeks=0)
+date2 = date - date_object
+
 print(date)
 print(date2)

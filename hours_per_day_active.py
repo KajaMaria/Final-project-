3e250a3757731d9b718cd507b2c126908cdf025c
Yaara K <yaarak@protonmail.com>
2019-09-05 21:38:29 +0100

    Filter by number of hours per day user is active

---
+++ hours_per_day_active.py
@@ -0,0 +1,50 @@
+import datetime
+from twitterapi import run_twitter_query, get_tweets_for_user 
+
+QUERY_TWEETS_COUNT_PER_USER = 20
+
+def convert_timestamp_to_datetime(created_time):
+  return datetime.datetime.strptime(created_time,"%a %b %d %H:%M:%S +0000 %Y")
+
+def map_activity_hours(tweets_timestamps):
+  activity_map = {}
+  for timestamp in tweets_timestamps:
+    created_at = convert_timestamp_to_datetime(timestamp)
+    created_at_date = created_at.date()
+    if created_at_date in activity_map:
+      activity_map[created_at_date].add(created_at.hour)
+    else:
+      activity_map[created_at_date] = set([created_at.hour])
+  return activity_map
+  
+def calculate_average_activity_hours_per_day(activity_dict):#tweets_timestamps):
+  total_hours = 0
+  for day,hours in activity_dict.items():
+    total_hours += len(hours)
+  active_days_count = len(activity_dict.keys())
+  average_hours_per_day = total_hours / active_days_count
+  return active_days_count, average_hours_per_day
+
+def get_tweets_timestamp(tweets):
+  timestamps = []
+  for tweet in tweets:
+    timestamps.append(tweet.created_at)
+  return timestamps
+
+def get_user_activity_hours_per_day(start_date, end_date,user_id):
+  tweets = get_tweets_for_user(start_date,end_date,user_id,QUERY_TWEETS_COUNT_PER_USER)
+  timestamps = get_tweets_timestamp(tweets)
+  activity_dict = map_activity_hours(timestamps)
+  return calculate_average_activity_hours_per_day(activity_dict)
+
+def filter_by_activity_hours_per_day(users,start_date,end_date):
+  activity_info_per_user = {}
+  total_number_of_days = (start_date - end_date)
+  for user in users:
+    active_days_count, average_hours_per_day = get_user_activity_hours_per_day(start_date,end_date,user['id'])
+    activity_info_per_user[user['screen_name']] = {'user_id': user['id'], 'total_number_of_days': total_number_of_days, 'active_days_count': active_days_count, 'average_hours_per_day': average_hours_per_day}
+  return activity_info_per_user
+
+users = run_twitter_query()
+print(filter_by_activity_hours_per_day(users,0,0))
+
--- twitterapi.py
+++ twitterapi.py
@@ -1,9 +1,8 @@
 import os
 import twitter
 import credentials
-import json
 import time
-import news
+from news import headlines
 
 # Init
 API_CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
@@ -23,32 +22,21 @@ def twitter_search_headline():
 
     results = {}
 
-    for headline in news.article_title:
+    for headline in headlines:
         results[headline] = api.GetSearch(
             term=headline, count=100, return_json=True)
     return results
 
 
-# def twitter_search_headline_user():
-
-#     output = {}
-
-#     for k, v in results.items():
-#         users = []
-#         for i in v['statuses']:
-#             users.append(i['user']['screen_name'])
-#         output[k] = users
-
-
-def twitter_search_users():
+def twitter_search_users(retweeted_headlines):
 
     users_by_headline = []
-    results = twitter_search_headline()
-
-    for k, v in results.items():
+ 
+    for headline, results in retweeted_headlines.items():
         user = {}
-        for tweet in v['statuses']:
+        for tweet in results['statuses']:
             user.update({'screen_name': tweet['user']['screen_name'],
+                         'id': tweet['user']['id'],
                          'urls': tweet['entities']['urls'],
                          'verified': tweet['user']['verified'],
                          'protected': tweet['user']['protected'],
@@ -58,12 +46,14 @@ def twitter_search_users():
         if len(user.keys()) > 0:
             users_by_headline.append(user)
 
-    print(users_by_headline)
     return users_by_headline
 
 
 def run_twitter_query():
-    return twitter_search_users()
+  retweeted_headlines = twitter_search_headline()
+  return twitter_search_users(retweeted_headlines)
 
+def get_tweets_for_user(start_date,end_date,user_id,count):
+  return api.GetUserTimeline(user_id=user_id,count=count)
 
-run_twitter_query()
+#run_twitter_query()

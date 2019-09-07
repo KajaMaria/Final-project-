fa18d724e0646b731ea24d567b51d6860b64eb66
Yaara K <yaarak@protonmail.com>
2019-09-08 00:48:23 +0100

    remove usertester from repo

--- bot_characteristics.py
+++ bot_characteristics.py
@@ -1,7 +1,7 @@
-from twitterapi import get_user
+from twitterapi import get_tweets_for_user
 
 
-def timeline_characteristics_filter():
+def timeline_characteristics():
 
     account_timeline = get_tweets_for_user(937962626222497793, 2)
 
@@ -13,4 +13,5 @@ def timeline_characteristics_filter():
 # identify repeating hashtags /
 # 3-5 keywords in tweets to suggest bot's tendencies.
 
-timeline_characteristics_filter()
+timeline_characteristics()
+

f20f4401cc731d1e27be5cee86d7998dce0f98d4
Yaara K <yaarak@protonmail.com>
2019-09-07 02:22:02 +0100

    add postgres module for storing suspected dummy news sites

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 import os
-
+ 
 os.environ['CONSUMER_KEY'] = ''
 os.environ['CONSUMER_SECRET'] = ''
 os.environ['ACCESS_TOKEN_KEY'] = ''
@@ -8,3 +8,7 @@ os.environ['NEWS_KEY'] = ''
 os.environ['GRAPHENEDB_BOLT_USER'] = ''
 os.environ['GRAPHENEDB_BOLT_PASSWORD'] = ''
 os.environ['GRAPHENEDB_BOLT_URL'] = ''
+os.environ['PGHOST'] = ''
+os.environ['PGDATABASE'] = ''
+os.environ['PGPASSWORD'] = ''
+os.environ['PGUSER'] = ''

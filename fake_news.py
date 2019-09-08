82ffa9cb50ce2ba900470a1d2ae6a82cc34eb88c
djvcom <djverrall@gmail.com>
2019-09-08 12:40:32 +0100

    finished filter for fake news

--- fake_news.py
+++ fake_news.py
@@ -7,6 +7,7 @@ from twitterapi import run_twitter_query, twitter_search_users
 
 keywords = ["cookie", "copyright policy", "Data Policy", "Subscriber Agreement", "Your Ad Choices", "Site Feedback", "Advertising", "Careers",
             "Guidelines", "Terms of Use", "Privacy Policy", "Accessibility Help", "Parental Guidance", "Get Personalised Newsletters", "Risk Management Solutions"]
+# keywords = ["sample"]
 only_links = SoupStrainer("a")
 
 
@@ -35,7 +36,6 @@ def find_links(user):
 
 
 def get_suspicious_sites(keywords, site):
-    # keywords_hash = { keyword.lower() : 0 for keyword in keywords }
     for link in site['links']:
         safeword = 0
         for keyword in keywords:
@@ -46,18 +46,28 @@ def get_suspicious_sites(keywords, site):
         return site
 
 
-
 def get_sites_with_user_mentioned(page_links, user):
     links = page_links['links']
     screen_name = user['screen_name']
-    for link in links: 
+    for link in links:
         if screen_name in link['text']:
             return {'screen_name': screen_name, 'url': page_links['url']}
+
+
+def filter_by_site_links(user):
+    site = find_links(user)
+    if get_sites_with_user_mentioned(site, user) != None:
+        print('failed at first method')
+        return True
+    else:
+        if get_suspicious_sites(keywords, site) != None:
+            print('failed at second method')
+            return True
+        else:
+            return False
 
 
 users = run_twitter_query()
 
 for user in users:
-    page_links = find_links(user)
-    print(get_sites_with_user_mentioned(page_links={'url': 'dodgy.com', 'links':[{'text': 'kaja'}]}, user={'screen_name': 'kaja'}))
-
+    print(filter_by_site_links(user))

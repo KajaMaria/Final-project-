97d5211e0f3288e625c72f764140511104f82b4a
Kaja <catcalledkaja@gmail.com>
2019-09-10 11:31:17 +0100

    adding landling

--- react/src/App.js
+++ react/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Layout, Textfield, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from './components/main'
+import { Layout, Footer, Textfield, FooterSection, FooterLinkList, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import classnames from "classnames";
 
 class App extends React.Component { 
   render() {
     return (
-      <div style={{height: '1000px', position: 'relative'}}>
+      <div style={{height: '100vh', position: 'relative'}}>
+        <div class="demo-layout-transparent mdl-layout mdl-js-layout">
           <Layout fixedHeader fixedDrawer>
-              <Header title="Title">
+             <Header transparent title="Botter" style={{color: 'white'}}>
                   <Textfield
                       value=""
                       onChange={() => {}}
@@ -17,12 +19,24 @@ class App extends React.Component {
                       expandableIcon="search"
                   />
               </Header>
-              <Drawer title="Title">
-                  <Navigation>
-                      <a href="#">Link</a>
-                      <a href="#">Link</a>
-                      <a href="#">Link</a>
-                      <a href="#">Link</a>
+              
+              <Drawer title="Go Fish">
+                  <Navigation >
+                      <a href="#">After Bot Identification Criteria</a>
+                      <Navigation>
+                      <a href="#">Frequency</a>
+                      <a href="#">Tweets to Age Ratio</a>
+                      <a href="#">Hours of Activity per day</a>
+                    </Navigation>
+                      <a href="#">After Scoring Criteria</a>
+                      <Navigation >
+                      <a href="#">Frequency Weight</a>
+                      <a href="#">Tweets to Age Ratio Weight</a>
+                      <a href="#">Hours of Activity per day Weight</a>
+                    </Navigation>
+                      <a href="#">Choose search start point</a>
+                      <a href="#">Bot Graveyard</a>
+                      <a href="#">Hours of Activity per day Weight</a> 
                   </Navigation>
               </Drawer>
               <Content>
@@ -31,6 +45,18 @@ class App extends React.Component {
               </Content>  
           </Layout>
       </div>
+
+      <Footer size="mini">
+    <FooterSection type="left" logo="Title">
+        <FooterLinkList>
+            <a href="#">Help</a>
+            <a href="#">Privacy & Terms</a>
+        </FooterLinkList>
+    </FooterSection>
+</Footer>
+      </div>
+    
+   
 
     );
   }
--- react/src/components/landingpage.js
+++ react/src/components/landingpage.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
         return(
-            <div><h1>Hello World</h1></div>
+            <div style={{width: '100%', margin: "auto"}}>
+                <Grid className="landing-grid">
+                   
+                </Grid>
+            </div>
         )
     }
 }

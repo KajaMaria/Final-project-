55013bc1e8a4e5165fd41ba460d66348c90c643e
Kaja <catcalledkaja@gmail.com>
2019-09-11 12:24:57 +0100

    testing verified filter

--- credentials.py
+++ credentials.py
@@ -1,1 +1,13 @@
 import os
+os.environ['CONSUMER_KEY'] = 'yKFcz1keZgBlu4n0GBoRGI7MI'
+os.environ['CONSUMER_SECRET'] = 'V1kcfbnrK6wj0Ur5QeVfX6Rx0VZ1uif8juz37NmtNKwgJOqtoi'
+os.environ['ACCESS_TOKEN_KEY'] = '1133592691118358528-GhB7pISQIV7xPTLvA8rO982wxnIaag'
+os.environ['ACCESS_TOKEN_SECRET'] = 'okZpj4NKMhTQOpfxue4xtQTwc1MKyEgqNhGdHR33OR0Dr'
+os.environ['NEWS_KEY'] = 'a0a5721dc4404755a8066dca7b386a21'
+os.environ['GRAPHENEDB_BOLT_USER'] = 'neo4j'
+os.environ['GRAPHENEDB_BOLT_PASSWORD'] = 'neo5j'
+os.environ['GRAPHENEDB_BOLT_URL'] = 'bolt://localhost:7687'
+os.environ['PGHOST'] = 'localhost'
+os.environ['PGDATABASE'] = 'pry'
+os.environ['PGPASSWORD'] = ''
+os.environ['PGUSER'] = 'daniel'
--- test_verified_filter.py
+++ test_verified_filter.py
@@ -1,18 +1,11 @@
-from filters.average_tweets_per_day_filter_file import  get_average_tweets_per_day, average_tweets_per_day_filter
+from filters.verified_filter import  unverified_and_unprotected_user_filter
 
 
 bot = {'screen_name': 'TestUserBot', 'id': 10000000000, 'urls': [{'url': 'www.texsaspost.com', 'expanded_url': 'https://texsaspost.com/', 'display_url': 'texsaspost.com', 'indices': [70, 93]}], 'verified': False, 'protected': False, 'created_at': 'Sun Sep 1 16:07:52 +0000 2019', 'statuses_count': 30745, 'description': 'Official account for global news, trends & analysis from the radio team behind World Business Report, Business Matters, Business Daily & Marketplace'}
 notbot = {'screen_name': 'TestUserNotBot', 'id': 10000000001, 'urls': [{'url': 'https://bbc.co.uk', 'expanded_url': 'https://bbc.co.uk', 'display_url': 'bbc.co.uk', 'indices': [74, 97]}], 'verified': True, 'protected': True, 'created_at': 'Sun Sep 1 10:18:15 +0000 2019', 'statuses_count': 51, 'description': 'For the latest updates on breaking news visit our website: https://t.co/5os3efxRyQ'}
 
-def test_average_tweets_per_day():
-    total = get_average_tweets_per_day(bot)
-    assert total == 3074.5
-    result = get_average_tweets_per_day(notbot)
-    assert result == 5.1
-
-
-def test_average_tweets_per_day_filter():
-     average = average_tweets_per_day_filter(bot)
-     assert average == True
-     average_notbot = average_tweets_per_day_filter(notbot)
-     assert average_notbot == False
+def test_unverified_and_unprotected_user_filter():
+    total = unverified_and_unprotected_user_filter(notbot)
+    assert total == False
+    total = unverified_and_unprotected_user_filter(bot)
+    assert total == True

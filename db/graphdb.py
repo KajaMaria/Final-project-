7f0e3222cb7c77e419ca24ddc78f6981a7756eff
djvcom <djverrall@gmail.com>
2019-09-11 15:40:58 +0100

    added store and retrieve from db

--- db/graphdb.py
+++ db/graphdb.py
@@ -1,9 +1,11 @@
-import credentials
+
 from neo4j import GraphDatabase
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.path.pardir))
-
+import credentials
+from db.redis_cache import retrieve_headlines
+from external_api.twitter_api import twitter_search_users_by_headlines, twitter_search_tweets_by_headlines
 
 GRAPHENEDB_BOLT_USER = os.environ.get('GRAPHENEDB_BOLT_USER')
 GRAPHENEDB_BOLT_PASSWORD = os.environ.get('GRAPHENEDB_BOLT_PASSWORD')
@@ -16,14 +18,15 @@ session = driver.session()
 
 
 def add_user_node(tx, user, source):
-    tx.run("MERGE (b:Bot {screen_name: $user.screen_name, id: $user.id, age: $user.created_at, score: $user.score, average_tweets: $user.average_tweets, followers: $user.followers, total_tweets: $user.total_tweets, status: $user.status}) "
-           "MERGE (s:Source {source: $source.source, content: $source.content}) "
-           "MERGE (b)-[:TWEETED {created_at: $text_string.created_date}]->(t) ",
+    # tx.run("MERGE (b:Bot {screen_name: $user.screen_name, id: $user.id, age: $user.created_at, score: $user.score, average_tweets: $user.average_tweets, followers: $user.followers, total_tweets: $user.total_tweets, status: $user.status}) "
+    #     #    "MERGE (s:Source {source: $source.source, content: $source.content}) "
+    #        "MERGE (b)-[:TWEETED {created_at: $text_string.created_date}]->(t) ",
+    #        user=user)
+    text_source = "MERGE (s:Source { text: $source }) "
+    tx.run("MERGE (a:User {screen_name: $user['screen_name'], id: $user['id'], created_at: $user['created_at'], followers_count: $user['followers_count'], statuses_count: $user['statuses_count']})"#, score: $user['score'], average_tweets: $user['average_tweets'] status: $user['status']}) ",
+           f"{text_source}"
+           "MERGE (a)-[:TWEETED]->(s)",
            user=user, source=source)
-    # tx.run("MERGE (a:User {name: $name}) ",
-    #        #    "MERGE (h:Headline {headline: $headline}) "
-    #        #    "MERGE (a)-[:RETWEETED]->(h)",
-    #        name=name)
 
 
 def add_text_node(tx, source):
@@ -35,6 +38,15 @@ def add_hashtag_node(tx, source):
            source=source)
 
 def add_relationship_node(tx, args):
+    pass
+
+def get_bots_and_following_relationships(tx):
+    # user_statement = "MATCH (u:User) RETURN u"
+    # build_relationships = "MATCH (u)-[:FOLLOWING]->(u) RETURN u"
+    labels_data = get_bots(tx)
+    relationships_data = get_relationships(tx)
+    return template_function(labels_data, relationships_data)
+    # label = [for label in labels_data]
     pass
 
 def retrieve_all_data(tx):
@@ -57,6 +69,38 @@ def retrieve_hashtag(tx, hashtag):
     for record in tx.run(statement, text=hashtag):
         return record
 
+def get_oldest(tx):
+    statement = "MATCH (u:User) RETURN ORDER BY u.created_at DESC LIMIT 1"
+    pass
+
+def get_bots(tx):
+    records = tx.run("MATCH (u:User) RETURN u.screen_name as screen_name, u.id as id, u.created_at as created_at, u.followers_count as followers_count, u.statuses_count as statuses_count")#u.score as score, u.status as status, u.average_tweets as average_tweets")
+    users = []
+    for record in records:
+        user = {
+            "User": {
+                "screen_name": record["screen_name"],
+                "id": record["id"],
+                "created_at": record["created_at"],
+                "score": record["score"],
+                "average_tweets": record["average_tweets"],
+                "followers_count": record["followers_count"],
+                "statuses_count": record["statuses_count"]
+                # "status": record["status"]
+            }
+        }
+        users.append(user)
+    return users
+        # for i in record.items()[0][1].items():
+        #     print(i[0], i[1])
+    # something = [i['n'].items() for i in records]
+    # for some in something:
+    #     print(some.value())
+    # for record in tx.run("MATCH (n) RETURN n"):
+    #     for i, j in iter(record['n']):
+    #         print(j)
+        
+
 def store_data(tx, function_name, data):
     function = {
         'user': add_user_node,
@@ -75,3 +119,22 @@ def store_data(tx, function_name, data):
     #        session.write_transaction(add_node_user, user['screen_name'])
     #    session.read_transaction(print_nodes)
 
+def relationships_data(tx):
+    
+    tx.run("MERGE (b)-[:TWEETED {created_at: $text_string.created_date}]->(t) ",)
+
+def template_function(labels_data, relationships_data):
+    return {
+        "labels": labels_data,
+        "relationships": relationships_data
+    }
+
+headlines = retrieve_headlines()
+tweeted_headlines = twitter_search_tweets_by_headlines(headlines)
+users = twitter_search_users_by_headlines(tweeted_headlines)
+# print(users[0])
+for user in users:
+    session.write_transaction(add_user_node, user[0], user[1])
+
+
+# print(session.read_transaction(print_nodes))
--- external_api/twitter_api.py
+++ external_api/twitter_api.py
@@ -1,6 +1,9 @@
-import os
-import twitter
+from db.redis_cache import store_users, retrieve_headlines
 import credentials
+import twitter
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.path.pardir))
 
 # Init
 API_CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
@@ -17,6 +20,7 @@ api = twitter.Api(consumer_key=(API_CONSUMER_KEY),
 
 DEFAULT_RETURNED_TWEETS = 100
 
+
 def twitter_search_tweets_by_headlines(headlines, count=DEFAULT_RETURNED_TWEETS):
     results = {}
     for headline in headlines:
@@ -24,12 +28,14 @@ def twitter_search_tweets_by_headlines(headlines, count=DEFAULT_RETURNED_TWEETS)
             term=headline, count=count, return_json=True)
     return results
 
+
 def twitter_search_users_by_tweets(tweeted_headlines):
     users_by_headline = []
 
     for headline, results in tweeted_headlines.items():
         user = {}
         for tweet in results['statuses']:
+            print(tweet['user'])
             user.update({'screen_name': tweet['user']['screen_name'],
                          'id': tweet['user']['id'],
                          'urls': tweet['entities']['urls'],
@@ -37,17 +43,37 @@ def twitter_search_users_by_tweets(tweeted_headlines):
                          'protected': tweet['user']['protected'],
                          'created_at': tweet['user']['created_at'],
                          'statuses_count': tweet['user']['statuses_count'],
+                         'followers_count': tweet['user']['followers_count'],
                          'description': tweet['user']['description']})
         if len(user.keys()) > 0:
             users_by_headline.append(user)
     return users_by_headline
 
+def twitter_search_users_by_headlines(tweeted_headlines):
+    users_by_headline = []
+
+    for headline, results in tweeted_headlines.items():
+        user = {}
+        for tweet in results['statuses']:
+            print(tweet['user'])
+            user.update({'screen_name': tweet['user']['screen_name'],
+                         'id': tweet['user']['id'],
+                         'urls': tweet['entities']['urls'],
+                         'verified': tweet['user']['verified'],
+                         'protected': tweet['user']['protected'],
+                         'created_at': tweet['user']['created_at'],
+                         'statuses_count': tweet['user']['statuses_count'],
+                         'followers_count': tweet['user']['followers_count'],
+                         'description': tweet['user']['description']})
+        if len(user.keys()) > 0:
+            users_by_headline.append((user, headline))
+    return users_by_headline
 
 def run_twitter_query(headlines):
   tweeted_headlines = twitter_search_tweets_by_headlines(headlines)
   users = twitter_search_users_by_tweets(tweeted_headlines)
   store_users(users)
-
+  return users
 
 def get_tweets_for_user(user_id, count, start_date=0, end_date=0):
     return api.GetUserTimeline(user_id=user_id, count=count)
@@ -65,4 +91,7 @@ def get_tweets_with_users(headlines):
   tweeted_headlines = twitter_search_tweets_by_headlines(headlines)
   users = twitter_search_users_by_tweets(tweeted_headlines)
   return users
+
+
+
 

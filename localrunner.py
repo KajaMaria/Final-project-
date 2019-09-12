5861943cb23238811124c1b5c79275d70693f96b
djvcom <djverrall@gmail.com>
2019-09-12 06:51:34 +0100

    created a wrapper file localrunner that allows us to quickly populate the db with users (note that I am NOT running any filters on the results, meaning there will be quite a large number of non-bots in the results) - this is purely so we can visualise the clusters of users around news stories and be able to guague the probability of bot interaction with certain news sources

---
+++ localrunner.py
@@ -0,0 +1,11 @@
+from external_api.news_api import get_headlines
+from external_api.twitter_api import run_twitter_query
+from db.redis_cache import retrieve_users
+from db.graphdb import add_user_node_source_headline
+
+def get_new_users_from_headlines_and_store_them():
+    get_headlines()
+    run_twitter_query()
+    users = retrieve_users()
+    for user in users:
+        add_user_node_source_headline(user)

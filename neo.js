3b00fba0ea4f75f15662d217a9680bc3eb2e9a8f
Ben D <benjamin.d928@icloud.com>
2019-09-12 12:32:20 +0100

    Fixed axios error in Neo4j(neovis) component

--- neo.js
+++ neo.js
@@ -11,38 +11,29 @@ export class Neo extends Component {
 
 
     fetchData() {
-        let configs = {
-            "Access-Control-Allow-Origin": "*"
-        }
 
-        axios.get('http://localhost:5000/', configs).then(response => {
-            this.setState({
-                neo_labels: response.data.label,
-                neo_realtionships: response.data.relationships,
-            })
+        let config = {
+            container_id: "viz",
+            server_url: "bolt://localhost:7687/",
+            server_user: "neo4j",
+            server_password: "dansbugs",
+            labels: {
+                "User": { name: "screen_name" }
+            },
+            relationships: {
+                TWEETED: ""
+            },
+            initial_cypher: "MATCH p=(:User)-[:TWEETED]->(:Source) RETURN p"
+        };
+        console.log(config.labels)
+        console.log(config.relationships)
 
-            let config = {
-                container_id: "viz",
-                server_url: "bolt://localhost:7687/",
-                server_user: "neo4j",
-                server_password: "dansbugs",
-                labels: {
-                    "User": { name: "screen_name" }
-                },
-                relationships: {
-                    TWEETED: ""
-                },
-                initial_cypher: "MATCH p=(:User)-[:TWEETED]->(:Source) RETURN p"
-            };
-            console.log(config.labels)
-            console.log(config.relationships)
+        var viz = new NeoVis(config);
+        console.log(viz)
+        console.log("Testing")
+        viz.render();
+        console.log("Testing 2")
 
-            var viz = new NeoVis(config);
-            console.log(viz)
-            console.log("Testing")
-            viz.render();
-            console.log("Testing 2")
-        })
     }
 
     componentDidMount() {
